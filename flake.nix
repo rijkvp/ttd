9dfd95fc181a63da76fd514e675cdef713e97d40
Rijk van Putten <rijk@rijkvp.nl>
2025-05-06 08:47:54 +0200

    Override latest Rust version

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     crane.url = "github:ipetkov/crane";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs =
     {
@@ -15,7 +19,7 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        craneLib = crane.mkLib pkgs;
+        craneLib = (crane.mkLib pkgs).overrideToolchain (p: p.rust-bin.stable.latest.default.override);
 
         commonArgs = {
           src = craneLib.cleanCargoSource ./.;
